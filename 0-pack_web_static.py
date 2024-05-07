4a3fdebc96d43212933e216219c44e157d8ab207
meliance <melakmu66@gmail.com>
2024-05-07 14:11:41 +0300

    fix

--- 0-pack_web_static.py
+++ 0-pack_web_static.py
@@ -11,17 +11,17 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
-  <head>
+    <head>
     </head>
-      <body>
-          Holberton School
-            </body>
-            </html>" | sudo tee /data/web_static/releases/test/index.html
+    <body>
+    Holberton School
+    </body>
+    </html>" | sudo tee /data/web_static/releases/test/index.html
 
-            sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+    sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-            sudo chown -R ubuntu:ubuntu /data/
+    sudo chown -R ubuntu:ubuntu /data/
 
-            sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+    sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-            sudo service nginx restart
+    sudo service nginx restart
