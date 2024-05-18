53158668cfd8e3187ed64b3a35aac5f4b7f565ff
meliance <melakmu66@gmail.com>
2024-05-18 15:04:41 +0300

    indentation corrected

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
-        """ Returns some text. """
-            return 'Hello HBNB!'
+    """ Returns some text. """
+    return 'Hello HBNB!'
 
-            if __name__ == '__main__':
-                app.run(host='0.0.0.0', port=5000)
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
