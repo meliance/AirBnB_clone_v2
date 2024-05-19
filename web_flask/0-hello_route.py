6473d7711cd72bcd9f4dc5d4c520fef1984ce3dc
meliance <melakmu66@gmail.com>
2024-05-19 15:33:35 +0300

    osd

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,5 +12,5 @@ def hello_world():
     """ Returns some text. """
     return 'Hello HBNB!'
 
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
