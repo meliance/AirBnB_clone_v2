6d21344cd5d674ea03ac865d655ff152e4267f1c
meliance <melakmu66@gmail.com>
2024-05-19 15:32:47 +0300

    corrected

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,10 +13,10 @@ def hello_world():
     return 'Hello HBNB!'
 
 
-    @app.route('/hbnb', strict_slashes=False)
-    def hello():
-        """ Return other text. """
-        return 'HBNB'
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """ Return other text. """
+    return 'HBNB'
 
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
