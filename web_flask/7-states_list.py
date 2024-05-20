60c284a80d962c9b0a7a18e49648b48c9d579f9d
meliance <melakmu66@gmail.com>
2024-05-20 14:00:39 +0300

    got

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-""" Start flask service that does something. """
+""" Start a Flask web application to list states from the database."""
 
-from flask import Flask
-from flask import render_template
+ from flask import Flask, render_template
+ from models import storage
+ from models.state import State
 
+ app = Flask(__name__)
+ app.url_map.strict_slashes = False
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+ @app.route('/states_list', strict_slashes=False)
+ def states_list():
+     """Display a HTML page with a list of states"""
+     states = sorted(storage.all(State).values(), key=lambda state: state.name)
+     return render_template('7-states_list.html', states=states)
 
-
-#@app.route()
-
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy session"""
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
